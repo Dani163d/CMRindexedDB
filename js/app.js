aec4231cfdabb911925b9ba29bc4195d5e896453
daniela <danielasilvacastillo163@gmail.com>
2024-02-24 18:41:10 -0400

    se creo la BD

---
+++ js/app.js
@@ -0,0 +1,36 @@
+(function() {
+
+    let DB;
+
+    document.addEventListener('DOMContentLoaded', () => {
+        crearDB();
+    });
+
+    // crear la BD de indexDB
+    function crearDB() {
+        const crearDB = window.indexedDB.open('crm', 1);
+
+        crearDB.onerror = function() {
+            console.log('hubo un error')
+        };
+
+        crearDB.onsuccess = function() {
+            DB = crearDB.result;
+        };
+
+        crearDB.onupgradeneeded = function(e) {
+            const db = e.target.result;
+
+            const objectStore = db.createObjectStore('crm', {keyPath: 'id', autoIncrement: true});
+
+            objectStore.createIndex('nombre', 'nombre', {unique: false});
+            objectStore.createIndex('correo', 'correo', {unique: true});
+            objectStore.createIndex('telefono', 'telefono', {unique: false});
+            objectStore.createIndex('empresa', 'empresa', {unique: false});
+            objectStore.createIndex('id', 'id', {unique: true});
+
+            console.log('DB lsita y creada')
+            
+        };
+    }
+}) ();
